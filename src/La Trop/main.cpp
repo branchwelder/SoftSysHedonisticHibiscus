fc103101d400ef6823eaff89385d5c189f9067e8
Sam Myers <sam@students.olin.edu>
2017-03-30 13:25:41 -0400

    Initialized XCode project.

---
+++ src/La Trop/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  La Trop
+//
+//  Created by Sam Myers on 3/30/17.
+//  Copyright © 2017 Hedonistic Hibiscus. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
