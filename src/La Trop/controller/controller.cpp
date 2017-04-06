b07f21a4873a1000242f4cd6f7bfb1d7104f59e2
Philip Seger <seger.philip@gmail.com>
2017-04-05 20:10:08 -0400

    ADD: freeglut dependencies

--- src/La Trop/controller/controller.cpp
+++ src/La Trop/controller/controller.cpp
@@ -16,19 +16,15 @@ void Controller::handleKeyboard(unsigned char key, int x, int y) {
             exit(0);
         case 'w':
             _model->movePlayer(0,1);
-            _view->render(); // Might want to change render function so it only updates screen/doesnt re-render everything
             break;
         case 'a':
             _model->movePlayer(-1,0);
-            _view->render();
             break;
         case 's':
             _model->movePlayer(0,-1);
-            _view->render();
             break;
         case 'd':
             _model->movePlayer(1,0);
-            _view->render();
             break;
         case 'q':
             std::cout << "fire left portal";
@@ -37,25 +33,23 @@ void Controller::handleKeyboard(unsigned char key, int x, int y) {
             std::cout << "fire right portal";
             break;
     }
+    _view->render();
 }
 
 void Controller::handleSpecialKeys(int key, int x, int y) {
     switch(key) {
         case GLUT_KEY_UP:
             _model->movePlayer(0,1);
-            _view->render();
             break;
         case GLUT_KEY_LEFT:
             _model->movePlayer(-1,0);
-            _view->render();
             break;
         case GLUT_KEY_DOWN:
             _model->movePlayer(0,-1);
-            _view->render();
             break;
         case GLUT_KEY_RIGHT:
             _model->movePlayer(1,0);
-            _view->render();
             break;
     }
+    _view->render();
 }
